20d299f3cb1b24474a42cdbd09fc44d57038e262
MyTim3130 <tin.hauslkramper@gmail.com>
2024-02-05 11:59:41 +0100

    donefr

--- app/page.tsx
+++ app/page.tsx
@@ -42,38 +42,40 @@ export default function Home() {
 
   return (
     <>
-    <Nav></Nav>
-          <video
-          autoPlay
-          loop
-          muted
-          className="-z-50 h-screen w-screen object-cover fixed opacity-50"
-        >
-          <source src="/images/texture.mp4" type="video/mp4" />
-        </video>
+      <Nav></Nav>
+      <video
+        autoPlay
+        loop
+        muted
+        className="-z-50 h-screen w-screen object-cover fixed opacity-50"
+      >
+        <source src="/images/texture.mp4" type="video/mp4" />
+      </video>
       <div className="App">
-        <AnimatedCursor
-          innerSize={20}
-          outerSize={50}
-          color="255, 255, 255"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={2}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-            '.hoverCursor'
-          ]}
-        />
+        {!showPhotography && (
+          <AnimatedCursor
+            innerSize={20}
+            outerSize={50}
+            color="255, 255, 255"
+            outerAlpha={0.2}
+            innerScale={0.7}
+            outerScale={2}
+            clickables={[
+              "a",
+              'input[type="text"]',
+              'input[type="email"]',
+              'input[type="number"]',
+              'input[type="submit"]',
+              'input[type="image"]',
+              "label[for]",
+              "select",
+              "textarea",
+              "button",
+              ".link",
+              ".hoverCursor",
+            ]}
+          />
+        )}
       </div>
       <div id="smooth-wrapper">
         <div id="smooth-content">
@@ -88,7 +90,6 @@ export default function Home() {
           <Skills></Skills>
           <Contact></Contact>
         </div>
-    
       </div>
     </>
   );
