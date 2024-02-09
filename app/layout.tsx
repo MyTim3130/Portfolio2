a5ad403885bd32a89fd725d41a375b1b64785116
MyTim3130 <tin.hauslkramper@gmail.com>
2024-02-09 11:42:50 +0100

    vercelData

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -7,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tim Hausl-Kramper || Portfolio",
-  description: "Tim Hausl Kramper"
+  description: "Tim Hausl Kramper",
 };
 
 export default function RootLayout({
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
